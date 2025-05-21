1cac75a4338856ffae39523efcdba902374da0aa
garg10aditya <117253638+garg10aditya@users.noreply.github.com>
2025-05-21 14:57:53 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://todolist-frontend-hsmy.onrender.com",
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
         credentials: "true"
     })
